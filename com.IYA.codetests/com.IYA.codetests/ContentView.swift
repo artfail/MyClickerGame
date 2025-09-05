033c2062f7849ed559eb9a5b3c167bf9667f1b25
John Bruneau <10361026+artfail@users.noreply.github.com>
2025-09-04 20:31:08 -0700

    Initial Commit

---
+++ com.IYA.codetests/com.IYA.codetests/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  com.IYA.codetests
+//
+//  Created by Bruneau on 9/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isOn = false
+    
+    var body: some View {
+        ZStack {
+            if !isOn{
+                Rectangle().background(Color.black)
+            }
+            
+            Button("Click Me") {
+                isOn.toggle()
+                onClick()
+            }.font(.largeTitle)
+        }
+    }
+    
+    func onClick() {
+       // do something
+        print("Click")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ com.IYA.codetests/com.IYA.codetests/com_IYA_codetestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_IYA_codetestsApp.swift
+//  com.IYA.codetests
+//
+//  Created by Bruneau on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct com_IYA_codetestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
